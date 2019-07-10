b9b25381c8a9def8850506457344a7adaa4aca8c
Glorings <18668465735@163.com>
2019-07-10 17:15:51 +0800

    init

---
+++ src/main/java/com/example/consulconfig/ConsulconfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.consulconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulconfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulconfigApplication.class, args);
+    }
+
+}
